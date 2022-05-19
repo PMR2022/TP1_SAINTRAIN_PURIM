17e239189ef3fcc1b1ff09110212cca98eead4b5
LucasSaintrain <lucasosaintrain@gmail.com>
2022-05-19 12:21:16 +0200

    Linked MainActivity button to ChoixListActivoty

--- app/src/main/java/com/example/tp1_pmr/MainActivity.kt
+++ app/src/main/java/com/example/tp1_pmr/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.tp1_pmr
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    var refBtnOK: Button? = null
+    var refEdtPseudo: EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        refEdtPseudo = findViewById(R.id.pseudo)
+
+        refBtnOK = findViewById<Button>(R.id.btnOK)
+        refBtnOK?.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when(view.id) {
+            R.id.btnOK -> {
+                val bundle = Bundle().apply {
+                    putString("pseudo", refEdtPseudo?.text.toString());
+                }
+                val choixListIntent = Intent(this,ChoixListActivity::class.java).apply {
+                    putExtras(bundle)
+                }
+                startActivity(choixListIntent);
+            }
+        }
     }
 }
