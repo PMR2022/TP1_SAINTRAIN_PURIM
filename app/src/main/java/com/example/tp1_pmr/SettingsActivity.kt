5e9c18a9b43f8b366b9e77558a1f757135e4a768
LucasSaintrain <lucasosaintrain@gmail.com>
2022-05-21 01:25:15 +0200

    SettingsActivity basic implementation

--- app/src/main/java/com/example/tp1_pmr/SettingsActivity.kt
+++ app/src/main/java/com/example/tp1_pmr/SettingsActivity.kt
@@ -1,11 +1,35 @@
 package com.example.tp1_pmr
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : PreferenceActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        addPreferencesFromResource(R.xml.prefs)
     }
 }
+
+/*class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+    }
+}*/
