69e1580439f1db2dca074b5d66bf488ef841bc0d
LucasSaintrain <lucasosaintrain@gmail.com>
2022-05-20 18:17:40 +0200

    Implemented ChoixListActivoty creation of new lists

--- app/src/main/java/com/example/tp1_pmr/ChoixListActivity.kt
+++ app/src/main/java/com/example/tp1_pmr/ChoixListActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -11,19 +13,28 @@ import androidx.recyclerview.widget.RecyclerView
 class ChoixListActivity : AppCompatActivity(), View.OnClickListener {
     val dataSet = mutableListOf("Liste 1","Liste 2","Liste 3")
 
+    var list : RecyclerView? = null
+    var refBtnOK: Button? = null
+    var refEdtNewList: EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choix_list)
 
-        val list = findViewById<RecyclerView>(R.id.list)
-        list.adapter = ListAdapter(dataSet)
-        list.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        list = findViewById(R.id.list)
+        list?.adapter = ListAdapter(dataSet)
+        list?.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        refEdtNewList = findViewById(R.id.newList)
+        refBtnOK = findViewById(R.id.btnOK)
+        refBtnOK?.setOnClickListener(this)
     }
 
     override fun onClick(view: View) {
         when (view.id){
             R.id.btnOK -> {
-                //...
+                dataSet.add(refEdtNewList?.text.toString())
+                list?.adapter?.notifyItemInserted(dataSet.size-1)
             }
         }
     }
